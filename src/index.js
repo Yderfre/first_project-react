89a29ed20ddc6651987fa0ad2026a22e1777f219
Fredy <fotoriver1@gmail.com>
2020-07-27 14:31:23 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const user = {
+    firstName: 'Fredy',
+    lastName: 'Rivera',
+    avatar:'https://i0.wp.com/www.bitme.gg/wp-content/uploads/2019/09/z-1-.jpg?w=1140&ssl=1'
+}
+
+function getName(user){
+    return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+    if(user){
+        return <h1> Hello {getName(user)}</h1>
+    
+    }
+    return <h1>Hello Stranger</h1>
+}
+
+
+
+const name = 'Fredy'
+
+const element = (
+    <div>
+        <h1>{getGreeting(user)}</h1>
+     <img src={user.avatar}/>
+    </div>
+)
+const container = document.getElementById('root')
+//ReactDOM.render(__QUE__,__DONDE__)
+ReactDOM.render(element, container)
